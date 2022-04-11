0371cdde58fe5eed7ff45ba257d801b3dcabcc8e
Vincent Kurniadjaja <calvinkurniadjaja@gmail.com>
2022-04-10 20:30:51 -0700

    implemented collisions

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,6 +5,7 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
     }
 
@@ -17,6 +18,13 @@ class Play extends Phaser.Scene {
 
         this.bg = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
 
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket');
+
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+
         //Green Rectangle
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
@@ -25,12 +33,39 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-
-        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket');
     }
 
     update() {
         this.bg.tilePositionX -= 4;
         this.p1Rocket.update();
+
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
+
+        //Checks collisions
+        if (this.checkCollisions(this.p1Rocket, this.ship03)) {
+            this.p1Rocket.reset();
+            this.ship03.reset();
+        }
+        if (this.checkCollisions(this.p1Rocket, this.ship02)) {
+            this.p1Rocket.reset();
+            this.ship02.reset();
+        }
+        if (this.checkCollisions(this.p1Rocket, this.ship01)) {
+            this.p1Rocket.reset();
+            this.ship01.reset();
+        }
+    }
+
+    checkCollisions (rocket, ship) {
+        if (rocket.x < ship.x + ship.width &&
+            rocket.x + rocket.width > ship.x &&
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship.y) {
+                return true;
+            } else {
+                return false;
+            }
     }
 }
